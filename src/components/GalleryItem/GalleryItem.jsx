dfa3154757c972eeb081652d87d924a214144f73
Brant McCarthy <brant.mccarthy@me.com>
2022-02-19 16:32:11 -0600

    added ternary click event to image

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,21 @@
-import './GalleryItem.css'
+import './GalleryItem.css';
+import { useState } from 'react';
 
 function GalleryItem({ image }) {
 
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        console.log('yo wuddup you clicked me');
+        setClicked(!clicked)
+    }
 
     return (
         <>
             <div className="flexbox-container-img">
-                <img src={image.path} />
+                <div onClick={handleClick}>
+                    {clicked ? <p>{image.description}</p> : <img src={image.path} />}
+                </div>
                 <button>love it!</button>
                 <p>No one love this :(</p>
             </div>
