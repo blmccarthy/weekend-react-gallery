2f1621f96f09229c24bf78837af74ae225254961
Brant McCarthy <brant.mccarthy@me.com>
2022-02-19 23:01:17 -0600

    succesfully migrated PUT functionality to use database

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -8,23 +8,39 @@ const pool = require('../modules/pool');
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for (const galleryItem of galleryItems) {
-        if (galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+    console.log('in PUT', req.params);
+    const galleryId = [req.params.id];
+    const sqlText = `UPDATE "gallery" SET "likes" = likes + 1 WHERE "id" = $1;`;
+    pool.query(sqlText, galleryId)
+    .then((dbRes) => {
+        console.log('in PUT .then', dbRes);
+        res.sendStatus(200);
+    }).catch((err) => {
+        console.log('in PUT .catch', err);
+        res.sendStatus(500);
+    })
+});
+
+// // [OLD] PUT Route
+
+// router.put('/like/:id', (req, res) => {
+//     console.log(req.params);
+//     const galleryId = req.params.id;
+//     for (const galleryItem of galleryItems) {
+//         if (galleryItem.id == galleryId) {
+//             galleryItem.likes += 1;
+//         }
+//     }
+//     res.sendStatus(200);
+// }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
     console.log('in GET');
-    let sqlText = `SELECT * FROM "gallery";`;
+    let sqlText = `SELECT * FROM "gallery" ORDER BY "id";`;
     pool.query(sqlText)
     .then((dbRes) => {
-        console.log('in GET .then', dbRes.rows);
+        console.log('in GET .then');
         res.send(dbRes.rows);
     }).catch((err) => {
         console.log('in GET .catch', err);
